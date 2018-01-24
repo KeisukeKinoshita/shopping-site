a905035c020a435883fc1f9b330a8e8ac26db8f4
KeisukeKinoshita <dolphinekicksuisui@yahoo.co.jp>
2018-01-24 12:29:19 +0000

    TOPページおおむね完成

---
+++ carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        nav:false,
+        dots: true,
+        autoplay:true
+        });
+});
+
